52db1fb5070d359057b527a40ff1c7caac43b1fe
Suresh Kanchamreddy <Suresh_Kanchamreddy@epam.com>
2018-07-17 18:19:56 +0530

    added book module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Book} from './models/book-model';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',  
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  books :  Book[] ;
+
+ constructor(){
+
+ }
+
+ngOnInit(){
+  this.books=[{
+    code:"001",
+    author:'oley',
+    name:"Angular 4",
+    thumbNail:"http://flatable.phoenixcoded.net/ltr/horizontal-static-icon/assets/images/user.png"
+},
+{
+  code:"002",
+  author:'oley 2',
+  name:"Angular 5",
+  thumbNail:"http://flatable.phoenixcoded.net/ltr/horizontal-static-icon/assets/images/user.png"
+}];
+
+}
+
+
 }
---
+++ src/app/models/book-model.ts
@@ -0,0 +1,7 @@
+export class  Book {
+    code:string;
+    author:string;
+    name:string;
+    thumbNail:string
+  };
+  
